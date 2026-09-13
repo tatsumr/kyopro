3fdc34c630b46c7539b8d0ae23dea9afe6ecf309
tatsumr <tatsuki.kawa8823@gmail.com>
2026-09-13 11:14:13 +0900

    Create gen.py

---
+++ debug/gen.py
@@ -0,0 +1,10 @@
+from gen_lib import *
+
+# 例: 1 以上 10 以下の整数 N を生成
+N = Int(1, 10)
+
+# 例: 長さ N、1 <= A[i] <= 20 の配列 A を生成
+A = Array(N, 1, 20)
+
+# 例: 長さ N の "ABC" からなる文字列 S を生成
+S = String(N, "ABC")
