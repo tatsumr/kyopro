53c06995c945034697460f80412bf6f86d7f98ee
tatsumr <tatsuki.kawa8823@gmail.com>
2026-09-13 11:20:16 +0900

    Update input.py

--- verify/ds/intervalset/randomtest/input.py
+++ verify/ds/intervalset/randomtest/input.py
@@ -1,1 +1,13 @@
+from gen_lib import *
 
+Q = Int(1, 20)
+print(Q)
+for _ in range(Q):
+    t = Int(1, 2)
+    l = Int(0, 5)
+    r = Int(l + 1, 10)
+    x = Int(0, 1)
+    if t == 1:
+        print(t, l, r, x)
+    else:
+        print(t, l, r)
